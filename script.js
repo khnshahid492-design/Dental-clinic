bdb6997f0c8f113ee17fe0d5cbf9872cb6273bf3
khnshahid492-design <khnshahid492@gmail.com>
2026-01-14 14:38:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("appointmentForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  // Redirect to thank you page
+  window.location.href = "thank-you.html";
+});
